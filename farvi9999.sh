d4bdfa788e74a934dec9e67a61de12f6dc3aa35b
guskant <gusni.kantu@gmail.com>
2015-06-09 12:51:54 +0000

    jmina lo cmavo be lo slabu selma'o

--- farvi9999.sh
+++ farvi9999.sh
@@ -3,4 +3,6 @@
 cp -R js ../gh-pages
 cp zantufa-0.9999.html ../gh-pages
 cp farvi.txt farvi-0.9999.txt
-grep -n "UTC_" zantufa-0.9999.js.peg >> farvi-0.9999.txt
+grep -n "UTC_" zantufa-0.9999.js.peg > temp
+awk '{print "zantufa-0.9999.js.peg:"$0}' temp >> farvi-0.9999.txt
+rm temp
