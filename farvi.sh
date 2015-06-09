bf9767dd9794358fc1ed3cba6d863356028ba946
guskant <gusni.kantu@gmail.com>
2015-06-09 04:05:48 +0000

    gau cnici

--- farvi.sh
+++ farvi.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-cp zantufa-0.9999.js.peg zantufa-`echo $1`.js.peg
-cp farvi.txt farvi-`echo $1`.txt
-grep -n "UTC_" zantufa-`echo $1`.js.peg >> farvi-`echo $1`.txt
-echo "------------------ mulno fa la zantufa_`echo $1` ------------------" >> farvi-`echo $1`.txt
+PEG="zantufa-"$1".js.peg"
+cp zantufa-0.9999.js.peg $PEG
+cp ./js/zantufa-0.9999.js ./js/zantufa-$1.js
+cp farvi.txt farvi-$1.txt
+grep -n "UTC_" $PEG > temp
+awk --assign awk_var="$PEG" '{print awk_var":"$0}' temp >> farvi-$1.txt
+echo "------------------ mulno fa la zantufa_$1 ------------------" >> farvi-$1.txt
+rm temp
+awk --assign awk_var="$1" '{gsub(/farvi-0.9999.txt/, "farvi.txt"); gsub(/0.9999/, awk_var); print $0}' zantufa-0.9999.html > zantufa-$1.html
 # uncomment for real release:
-# mv farvi-`echo $1`.txt farvi.txt
+# cp farvi-$1.txt farvi.txt
 # cp -R js ../gh-pages
-# cp zantufa-`echo $1`.html ../gh-pages
-# sed 's/UTC_/UTC/' zantufa-`echo $1`.js.peg > zantufa-0.9999.js.peg
+# cp zantufa-$1.html ../gh-pages
+# sed 's/UTC_/UTC/' $PEG > zantufa-0.9999.js.peg
+# git rm farvi-$1.txt zantufa-$1.html js/zantufa-$1.js
+#
+# uncomment to remove the version:
+# rm farvi-$1.txt zantufa-$1.html zantufa-$1.js.peg js/zantufa-$1.js
